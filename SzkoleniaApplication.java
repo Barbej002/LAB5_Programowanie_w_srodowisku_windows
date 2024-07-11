fc6b6947e4593c03ef93c5bf4bde5a4101e53b88
Bartosz Bejgrowicz <105065126+Barbej002@users.noreply.github.com>
2024-07-11 16:47:25 +0200

    Add files via upload

---
+++ SzkoleniaApplication.java
@@ -0,0 +1,12 @@
+package com.example.szkolenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzkoleniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzkoleniaApplication.class, args);
+    }
+}
